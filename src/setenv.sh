a0812fb12980f751094020bc87225aae10e5b532
xieguangtai <xieguangtai@outlook.com>
2019-12-07 13:44:49 +0800

    Update setenv.sh

--- src/setenv.sh
+++ src/setenv.sh
@@ -1,8 +1,8 @@
-export GITACCOUNT=
+export GITACCOUNT=xieguangtai
 
-export MYCLUSTER=
-export KUBECONFIG=
+export MYCLUSTER=tektonknativebeijing22
+export KUBECONFIG=/tmp/ic/AB/.bluemix/plugins/container-service/clusters/tektonknativebeijing22/kube-config-tok04-tektonknativebeijing22.yml
 
-export NAMESPACE=
-export EMAIL=
-export INGRESS=
+export NAMESPACE=jamous
+export EMAIL=jamous2018@gmail.com
+export INGRESS=tektonknativebeijing22-0e3e0ef4c9c6d831e8aa6fe01f33bfc4-0001.jp-tok.containers.appdomain.cloud
